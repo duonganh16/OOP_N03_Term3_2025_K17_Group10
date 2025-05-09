4f4f418a8e37a4b756757a446cbcd4256c2ab0bf
MaiAnhne <151895735+MaiAnhne@users.noreply.github.com>
2025-05-09 08:53:39 +0700

    Delete

--- Pet_Care_System/MedicalRecordTest.java
+++
@@ -1,6 +0,0 @@
-public class MedicalRecordTest {
-  public static void test() {
-    MedicalRecord record = new MedicalRecord("Buddy", "Vaccination", "Healthy", "2025-05-01");
-    record.displayInfo();
-  }
-}
