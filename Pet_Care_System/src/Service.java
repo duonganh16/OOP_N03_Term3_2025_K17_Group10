16b8e5e24b68440b1c52d6722fc9df3dcd9b5ebf
duonganh16 <23011500@st.phenikaa-uni.edu.vn>
2025-05-15 08:10:35 +0700

    Update Service.java

--- Pet_Care_System/src/Service.java
+++ Pet_Care_System/src/Service.java
@@ -1,14 +1,10 @@
-public class Service {
-    private String serviceName;
-    private double price;
+class Service {
+    String serviceName;
+    double price;
 
-    public Service(String serviceName, double price) {
+    Service(String serviceName, double price) {
         this.serviceName = serviceName;
         this.price = price;
-    }
-
-    public void displayService() {
-        System.out.println("Service: " + serviceName + " - Price: $" + price);
     }
 }
 
