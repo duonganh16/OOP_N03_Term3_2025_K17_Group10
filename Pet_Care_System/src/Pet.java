3bec5a90a4b711111e7a9e9be5d338569fef3949
MaiAnhne <151895735+MaiAnhne@users.noreply.github.com>
2025-05-15 08:11:42 +0700

    Pet.java

--- Pet_Care_System/src/Pet.java
+++ Pet_Care_System/src/Pet.java
@@ -1,18 +1,10 @@
-public class Pet {
-    private String name;
-    private String species;
-    private int age;
+class Pet {
+    String name;
+    String species;
+    int age;
 
-    public Pet(String name, String species, int age) {
-        this.name = name;
-        this.species = species;
-        this.age = age;
-    }
-
-    public void showInfo() {
-        System.out.println("Pet Name: " + name);
-        System.out.println("Species: " + species);
-        System.out.println("Age: " + age);
-    }
+public Pet(String name, String species, int age) {
+    this.name = name;
+    this.species = species;
+    this.age = age;
 }
-
