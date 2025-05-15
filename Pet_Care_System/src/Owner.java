63ee38ef76e00f0a33e0efd635b4b1ce84724dab
MaiAnhne <151895735+MaiAnhne@users.noreply.github.com>
2025-05-15 08:12:39 +0700

    Owner.java

--- Pet_Care_System/src/Owner.java
+++ Pet_Care_System/src/Owner.java
@@ -1,15 +1,9 @@
-public class Owner {
-    private String name;
-    private String phoneNumber;
+class Owner {
+    String name;
+    String phoneNumber;
 
-    public Owner(String name, String phoneNumber) {
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public void showInfo() {
-        System.out.println("Owner Name: " + name);
-        System.out.println("Phone: " + phoneNumber);
-    }
+Owner(String name, String phoneNumber) {
+    this.name = name;
+    this.phoneNumber = phoneNumber;
 }
 
