b812c35aebd5b0e30c1045dbbcf5bbefb7ff419f
MaiAnhne <151895735+MaiAnhne@users.noreply.github.com>
2025-05-15 08:10:51 +0700

    Product.java

--- Pet_Care_System/src/Product.java
+++ Pet_Care_System/src/Product.java
@@ -1,30 +1,10 @@
-public class Product {
-private String name;
-private double price;
-private String description;
+ class Product {
+   String name;
+   double price;
+   String description;
 
-public Product(String name, double price, String description) {
-this.name = name;
-this.price = price;
-this.description = description;
-}
-
-public String getName() {
-return name;
-}
-
-public double getPrice() {
-return price;
-}
-
-public String getDescription() {
-return description;
-}
-
-public void displayInfo() {
-System.out.println("Product Information:");
-System.out.println("Name: " + name);
-System.out.println("Price: $" + price);
-System.out.println("Description: " + description);
-}
+ Product(String name, double price, String description) {
+   this.name = name;
+   this.price = price;
+   this.description = description;
 }
