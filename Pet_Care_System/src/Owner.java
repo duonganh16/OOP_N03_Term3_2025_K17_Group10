175f087bd198cf0b486ea5973719db35d0f37b31
MaiAnhne <151895735+MaiAnhne@users.noreply.github.com>
2025-05-16 01:19:52 +0000

    Update Owner.java: thêm setter cho email

--- Pet_Care_System/src/Owner.java
+++ Pet_Care_System/src/Owner.java
@@ -1,9 +1,49 @@
-class Owner {
-    String name;
-    String phoneNumber;
+public class Owner {
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private String email;
 
-Owner(String name, String phoneNumber) {
-    this.name = name;
-    this.phoneNumber = phoneNumber;
+    public Owner() {
+    }
+
+    public Owner(String name, String address, String phoneNumber, String email) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
 
