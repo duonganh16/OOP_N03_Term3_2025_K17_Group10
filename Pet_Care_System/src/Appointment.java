762796c9c793c296a2070f904e139ef7ecc77cc1
duonganh16 <23011500@st.phenikaa-uni.edu.vn>
2025-05-16 08:06:16 +0700

    Appointment.java

--- Pet_Care_System/src/Appointment.java
+++ Pet_Care_System/src/Appointment.java
@@ -1,15 +1,56 @@
-class Appointment {
-  String petName;
-  String ownerName;
-  String date;
-  String time;
-  String service;
+public class Appointment {
+    private String petName;
+    private String ownerName;
+    private String date;
+    private String time;
+    private String service;
 
-Appointment(String petName, String ownerName, String date, String time, String service) {
-  this.petName = petName;
-  this.ownerName = ownerName;
-  this.date = date;
-  this.time = time;
-  this.service = service;
+    public Appointment(String petName, String ownerName, String date, String time, String service) {
+        this.petName = petName;
+        this.ownerName = ownerName;
+        this.date = date;
+        this.time = time;
+        this.service = service;
+    }
+
+    // Getters
+    public String getPetName() {
+        return petName;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getService() {
+        return service;
+    }
+
+    public void setPetName(String petName) {
+        this.petName = petName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
 }
-
