bd35072a6f6a76c8bc12e0a9ff79378e26e52165
Doraemon <23010020@st.phenikaa-uni.edu.vn>
2025-05-22 08:35:03 +0700

    Service.java

--- Pet_Care_System/src/Service.java
+++ Pet_Care_System/src/Service.java
@@ -1,10 +1,39 @@
-class Service {
-    String serviceName;
-    double price;
+public class Service {
+    private int id; // Thêm id
+    private String serviceName;
+    private double price;
 
-    Service(String serviceName, double price) {
+    // Constructor có id
+    public Service(int id, String serviceName, double price) {
+        this.id = id;
         this.serviceName = serviceName;
         this.price = price;
     }
-}
 
+    // Getter và Setter cho id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter và Setter cho serviceName
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    // Getter và Setter cho price
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
