b83626e0f8a076c0058c75a2847dd8803e55fb0a
MaiAnhne <151895735+MaiAnhne@users.noreply.github.com>
2025-06-28 01:53:41 +0000

    m

---
+++ gs-serving-web-content-main/complete/src/main/java/com/example/servingwebcontent/Test/test/AppointmentTest.java
@@ -0,0 +1,6 @@
+public class AppointmentTest {
+  public static void test() {
+    Appointment appointment = new Appointment("Buddy", "Dr. Smith", "2025-05-15", "10:00 AM");
+    appointment.displayInfo();
+  }
+}
---
+++ gs-serving-web-content-main/complete/src/main/java/com/example/servingwebcontent/Test/test/OwnerTest.java
@@ -0,0 +1,7 @@
+public class OwnerTest {
+    public static void test() {
+        Owner owner = new Owner("Anna", "0123456789");
+        owner.showInfo();
+    }
+}
+
